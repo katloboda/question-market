401e864ca51a305744940eb9a051303332ac8428
hansss <hanakim@college.harvard.edu>
2015-04-22 22:16:23 -0400

    Able to delete questions, but hitting a 500 error

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,13 +4,25 @@ class QuestionsController < ApplicationController
   end
 
   def create
+    logger.debug "PARAMS for create: #{params}"
     question = Question.new(params.require(:question).permit(:text, :user_id))
+    logger.debug "QUESTION created: #{question}"
+    logger.debug "ID of Question created: #{question.id}"
     params[:answers].each do |a|
       question.answers << Answer.new(a.permit(:text))
     end
     question.save
     # Does this need .to_json() if it is called as render json: ?
     render json: question.to_json(:include => :answers)
+  end
+
+
+  def destroy
+    logger.debug "PARAMS for destroy: #{params}"
+    @question = Question.find(params[:id])
+    logger.debug "QUESTION for destroy: #{@question}"
+    @question.destroy
+
   end
 
 end
