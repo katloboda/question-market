753f4ccfdb4591aadd35acb43d1be30784acb0fc
hansss <hanakim@college.harvard.edu>
2015-04-30 14:58:44 -0400

    Fixes to logger debugs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,20 +4,18 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    logger.debug "PARAMS for create: #{params}"
     question = Question.new(params.require(:question).permit(:text, :user_id))
-    logger.debug "QUESTION created: #{question}"
-    logger.debug "ID of Question created: #{question.id}"
     params[:answers].each do |a|
       question.answers << Answer.new(a.permit(:text))
     end
     question.save
+    logger.debug "QUESTION created: #{question}"
+    logger.debug "ID of Question created: #{question.id}"
     render json: question.to_json(:include => :answers)
   end
 
 
   def destroy
-    logger.debug "PARAMS for destroy: #{params}"
     @question = Question.find(params[:id])
     logger.debug "QUESTION for destroy: #{@question}"
     @question.destroy
